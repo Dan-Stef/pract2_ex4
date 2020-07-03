68d050cba6bb0a0732154d931da91c55f9e3e689
Даниил Стефаненков <daniilstef2391@gmail.com>
2020-07-03 11:08:57 +0300

    Добавьте файлы проекта.

---
+++ pract2_ex4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract2_ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
